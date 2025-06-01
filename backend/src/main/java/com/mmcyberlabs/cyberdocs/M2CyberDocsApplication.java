364eb84e71c5ab2b3e3e15ce0b457388017b9fbc
johnmartinez <jmartinez5120@gmail.com>
2025-06-01 11:35:30 -0400

    initial commit

---
+++ backend/src/main/java/com/mmcyberlabs/cyberdocs/M2CyberDocsApplication.java
@@ -0,0 +1,13 @@
+package com.mmcyberlabs.cyberdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2CyberDocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(M2CyberDocsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/mmcyberlabs/cyberdocs/M2CyberDocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mmcyberlabs.cyberdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M2CyberDocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
